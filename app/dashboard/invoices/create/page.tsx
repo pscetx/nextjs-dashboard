30451fe1486ea85c7f21d538950cecc65a65a898
pscetx <pscetxvn@gmail.com>
2025-03-06 00:14:13 +0700

    ok

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,10 +1,6 @@
-import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
 
 export default async function Page() {
-  const customers = await fetchCustomers();
-
   return (
     <main>
       <Breadcrumbs
@@ -17,7 +13,6 @@ export default async function Page() {
           },
         ]}
       />
-      <Form customers={customers} />
     </main>
   );
 }
