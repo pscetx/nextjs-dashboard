b6717200df24f0a5d24c6aed91605e504a7ef9b3
pscetx <pscetxvn@gmail.com>
2026-01-20 20:51:02 +0700

    ok

--- app/work/fit-journey/page.tsx
+++ app/work/fit-journey/page.tsx
@@ -16,10 +16,10 @@ export default function Page() {
         ← back
       </button>
 
-      <div className="flex w-full flex-col space-y-4 text-lg text-gray-600 md:space-y-8">
+      <div className="flex w-full flex-col space-y-4 text-lg text-gray-600 md:space-y-6">
         <Image
-          className="w-full rounded-xl"
-          src="/fj-HeroImage.jpg"
+          className="mx-auto w-full rounded-xl"
+          src="/fj-Hero Image.jpg"
           width={1000}
           height={50}
           alt="Hero"
@@ -61,88 +61,87 @@ export default function Page() {
           </div>
         </div>
 
-        <h2 className="mt-20 text-3xl font-bold text-amber-800">Challenge</h2>
+        <h1 className="text-3xl font-bold">challenge</h1>
 
-        <p className="mt-2">
-          Users struggled to stay motivated and consistent, feeling overwhelmed
-          by choices and unable to see meaningful progress early on.
+        <p>
+          In traditional fitness and workout apps, users often lose motivation
+          due to overwhelming options, repetitive routines, and progress that
+          feels slow or intangible, leading to inconsistent usage.
         </p>
 
-        <h2 className="mt-20 text-3xl font-bold text-amber-800">Solution</h2>
+        <h1 className="text-3xl font-bold">solution</h1>
 
         <p className="mt-2">
-          Ads were moved to low-attention moments and redesigned as native,
-          clearly labeled units, improving CTR while controlling drop rate and
-          protecting retention and LTV.
+          We designed a gamified, challenge-based system with XP, missions, and
+          milestone rewards that make progress immediately visible and encourage
+          users to return and build lasting habits.
         </p>
-      </div>
+        <p className="mt-2">
+          FitJourney redefines the way you approach fitness by turning your
+          workout routine into an immersive game where every step forward is a
+          step up! Perfect for anyone looking to add a touch of fun and a
+          competitive edge to their fitness goals, the app combines effective
+          exercise with social interaction.
+        </p>
 
-      <Image
-        className="rounded-xl"
-        src="/fj-Overview.jpg"
-        width={1000}
-        height={50}
-        alt="Overview"
-        priority
-      />
-      <Image
-        className="rounded-xl"
-        src="/fj-Design System.jpg"
-        width={1000}
-        height={50}
-        alt="Design System"
-        priority
-      />
-      <Image
-        className="rounded-xl"
-        src="/fj-Components.jpg"
-        width={1000}
-        height={50}
-        alt="Components"
-        priority
-      />
-      <Image
-        className="rounded-xl"
-        src="/fj-Showcase 1.jpg"
-        width={1000}
-        height={50}
-        alt="Showcase #1"
-        priority
-      />
-      <Image
-        className="rounded-xl"
-        src="/fj-Showcase 2.jpg"
-        width={1000}
-        height={50}
-        alt="Showcase #2"
-        priority
-      />
-      <Image
-        className="rounded-xl"
-        src="/fj-Showcase 3.jpg"
-        width={1000}
-        height={50}
-        alt="Showcase #3"
-        priority
-      />
-      <div>
         <Image
-          className="rounded-xl"
-          src="/fj-Prototype.jpg"
+          className="mx-auto rounded-xl"
+          src="/fj-Overview.jpg"
           width={1000}
           height={50}
-          alt="Prototype"
+          alt="Overview"
           priority
         />
-        <FigmaEmbed figmaUrl="https://www.figma.com/proto/TodPd2jMFIJnpLEDqKQio5/FitJourney-App?node-id=16-45&p=f&t=ATN7GIXRND0ZjSIk-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=16%3A45" />
+
+        <h1 className="text-3xl font-bold">showcase</h1>
+
         <Image
-          className="rounded-xl"
-          src="/fj-Thank You.jpg"
+          className="mx-auto rounded-xl"
+          src="/fj-Showcase 1.jpg"
           width={1000}
           height={50}
-          alt="Thank You"
+          alt="Showcase #1"
           priority
         />
+
+        <Image
+          className="mx-auto rounded-xl"
+          src="/fj-Showcase 2.jpg"
+          width={1000}
+          height={50}
+          alt="Showcase #2"
+          priority
+        />
+
+        <Image
+          className="mx-auto rounded-xl"
+          src="/fj-Showcase 3.jpg"
+          width={1000}
+          height={50}
+          alt="Showcase #3"
+          priority
+        />
+
+        <div>
+          <Image
+            className="mx-auto rounded-xl"
+            src="/fj-Prototype.jpg"
+            width={1000}
+            height={50}
+            alt="Prototype"
+            priority
+          />
+
+          <FigmaEmbed figmaUrl="https://www.figma.com/proto/TodPd2jMFIJnpLEDqKQio5/FitJourney-App?node-id=16-45&p=f&t=ATN7GIXRND0ZjSIk-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=16%3A45" />
+          <Image
+            className="mx-auto rounded-xl"
+            src="/fj-Thank You.jpg"
+            width={1000}
+            height={50}
+            alt="Thank You"
+            priority
+          />
+        </div>
       </div>
     </div>
   );
