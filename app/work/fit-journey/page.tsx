5bad28b1ae6d4f1f688811f12c17b4c2e810e6c2
pscetx <pscetxvn@gmail.com>
2026-01-21 17:56:49 +0700

    ok

--- app/work/fit-journey/page.tsx
+++ app/work/fit-journey/page.tsx
@@ -73,7 +73,7 @@ export default function Page() {
           milestone rewards that make progress immediately visible and encourage
           users to return and build lasting habits.
         </p>
-        <p>
+        <p className="pb-2">
           FitJourney redefines the way you approach fitness by turning your
           workout routine into an immersive game where every step forward is a
           step up! Perfect for anyone looking to add a touch of fun and a
@@ -81,22 +81,22 @@ export default function Page() {
           exercise with social interaction.
         </p>
         <p
-          className={`${montserrat.className} text-md w-fit rounded-xl bg-sky-100 px-4 py-2 font-medium text-gray-800`}
+          className={`${montserrat.className} w-fit rounded-xl bg-sky-100 px-4 py-2 text-sm font-medium text-gray-800`}
         >
           Weekly Quests & Daily Missions
         </p>
         <p
-          className={`${montserrat.className} text-md w-fit rounded-xl bg-sky-100 px-4 py-2 font-medium text-gray-800`}
+          className={`${montserrat.className} w-fit rounded-xl bg-sky-100 px-4 py-2 text-sm font-medium text-gray-800`}
         >
           Connect and Compete with Friends
         </p>
         <p
-          className={`${montserrat.className} text-md w-fit rounded-xl bg-sky-100 px-4 py-2 font-medium text-gray-800`}
+          className={`${montserrat.className} w-fit rounded-xl bg-sky-100 px-4 py-2 text-sm font-medium text-gray-800`}
         >
           Personalized Avatars & Rewards
         </p>
         <p
-          className={`${montserrat.className} text-md w-fit rounded-xl bg-sky-100 px-4 py-2 font-medium text-gray-800`}
+          className={`${montserrat.className} w-fit rounded-xl bg-sky-100 px-4 py-2 text-sm font-medium text-gray-800`}
         >
           Motivation and Achievement Tracking
         </p>
